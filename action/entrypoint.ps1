2f80b571636cb2085b8329f39bb59c84c6763394
Rob Bos <raj.bos@gmail.com>
2021-04-09 22:03:16 +0200

    Logging the environment variable

--- action/entrypoint.ps1
+++ action/entrypoint.ps1
@@ -3,5 +3,6 @@ Write-Host $PSVersion
 Write-Host "Hello Octogotchi!"
 
 # curl post/get here, load repo name from param
+Write-Host "Repo slug from Powershell: $($env:GITHUB_REPOSITORY)"
 
 # act on the result
